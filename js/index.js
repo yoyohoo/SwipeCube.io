be1aa31d13483ebdfd7b08b322cade9414a9571c
yoyohoo <sinful_001@sina.com>
2016-12-20 17:14:01 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+	pagination: '.swiper-pagination',
+	effect: 'cube',
+	grabCursor: true,
+	cube: {
+		shadow: true,
+		slideShadows: true,
+		shadowOffset: 20,
+		shadowScale: 0.94
+	}
+});
